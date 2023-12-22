8ecdcf4fde267d6e17319d20ee0ed7c5f44df605
sameer6699 <sammyjadhav6699@gmail.com>
2023-12-22 12:55:12 +0530

    ejs-Login-Authentication

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.render("index.ejs");
+});
+
+app.get('/login', (req, res) => {
+    res.render("login.ejs");
+});
+
+app.listen(3000, () => {});
